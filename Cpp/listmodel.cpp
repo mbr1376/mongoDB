6e50c0bfbb881318d8eed6a5d15585fa50d8f7bd
mbr1376 <mbroodsaraby76@gmail.com>
2025-07-10 12:57:18 -0700

    create listmodel  for data mongog

---
+++ Cpp/listmodel.cpp
@@ -0,0 +1,32 @@
+#include "listmodel.h"
+
+ListModel::ListModel(QObject *parent)
+    : QAbstractListModel{parent}
+{}
+
+int ListModel::rowCount(const QModelIndex &parent) const
+{
+    return m_items.count();
+}
+
+QVariant ListModel::data(const QModelIndex &index, int role) const
+{
+    const Item &item = m_items[index.row()];
+    if (role == Qt::DisplayRole) return item.FirstName;
+    if (role == Qt::UserRole +1) return item.LastName;
+    if (role == Qt::UserRole +2) return item.PhoneNumber;
+    if (role == Qt::UserRole +3) return item.email;
+    if (role == Qt::UserRole +4) return item.id;
+
+    return {}
+
+}
+
+QHash<int, QByteArray> ListModel::roleNames() const
+{
+    return {{FirstName,"FirstName"},
+            {LastName,"LastName"},
+            {PhoneNumber,"PhoneNumber"},
+            {email,"email"},
+            {id,"id"}}
+}
---
+++ Cpp/listmodel.h
@@ -0,0 +1,41 @@
+#ifndef LISTMODEL_H
+#define LISTMODEL_H
+
+#include <QObject>
+#include <QAbstractListModel>
+struct Item
+{
+    QString FirstName;
+    QString LastName;
+    QString PhoneNumber;
+    QString email;
+    int  id;
+};
+class ListModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+
+public:
+    enum Roles{
+        FirstName,
+        LastName = Qt::UserRole +1,
+        PhoneNumber = Qt::UserRole +2,
+        email = Qt::UserRole +3,
+        id = Qt::UserRole +4
+    };
+    explicit ListModel(QObject *parent = nullptr);
+protected:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+
+signals:
+
+
+private:
+    QList<Item> m_items;
+};
+
+#endif // LISTMODEL_H
