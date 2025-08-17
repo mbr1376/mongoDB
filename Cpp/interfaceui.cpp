ac90bce879ceb27784fd75cf63602b9696b96a6c
mbr1376 <mbroodsaraby76@gmail.com>
2025-08-17 12:38:54 -0700

    complate add ui and display ui

--- Cpp/interfaceui.cpp
+++ Cpp/interfaceui.cpp
@@ -12,13 +12,25 @@ InterfaceUI::InterfaceUI(MongoManager* mongo,ListModel * listmodel,QObject *pare
 
 void InterfaceUI::get_data(const QString &fname, const QString &lname, const QString &phone, const QString &email)
 {
-    // bsoncxx::builder::stream::document doc{};
-    // doc << "id" << m_id
-    //     << "FirstName" << fname
-    //     << "LastName" << lname
-    //     << "PhoneNumber" << phone
-    // << "email" << email;
+     bsoncxx::builder::stream::document doc{};
+     doc << "id" << m_id
+         << "FirstName" << fname.toStdString()
+         << "LastName" << lname.toStdString()
+         << "PhoneNumber" << phone.toStdString()
+     << "email" << email.toStdString();
 
-    //monogo->addToDatabase(doc);
+    m_mongo->addToDatabase(doc);
+     QList<QVariantMap> listmap = m_mongo->data();
+    QList<Item> listItem;
+    foreach (QVariantMap var, listmap) {
+         Item item;
+         item.FirstName = var["FirstName"].toString();
+         item.LastName = var["LastName"].toString();
+         item.email = var["email"].toString();
+         item.PhoneNumber = var["PhoneNumber"].toString();
+         item.id = var["id"].toInt();
+         listItem.append(item);
+    }
+    m_listmodel->refreshFromMongo(listItem);
     //qDebug() << fname << lname << phone << email;
 }
