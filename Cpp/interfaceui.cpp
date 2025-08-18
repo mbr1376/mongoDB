4658b0ebd56246b763d4febd84d2eeb23cad6d84
mbr1376 <mbroodsaraby76@gmail.com>
2025-08-18 11:33:17 -0700

    complate app c++

--- Cpp/interfaceui.cpp
+++ Cpp/interfaceui.cpp
@@ -21,16 +21,58 @@ void InterfaceUI::get_data(const QString &fname, const QString &lname, const QSt
 
     m_mongo->addToDatabase(doc);
      QList<QVariantMap> listmap = m_mongo->data();
-    QList<Item> listItem;
-    foreach (QVariantMap var, listmap) {
-         Item item;
-         item.FirstName = var["FirstName"].toString();
-         item.LastName = var["LastName"].toString();
-         item.email = var["email"].toString();
-         item.PhoneNumber = var["PhoneNumber"].toString();
-         item.id = var["id"].toInt();
-         listItem.append(item);
-    }
+    QList<Item> listItem = convertMaptoItem(listmap);
+
     m_listmodel->refreshFromMongo(listItem);
     //qDebug() << fname << lname << phone << email;
+}
+
+void InterfaceUI::search(const QString &txt)
+{
+    QList <QVariantMap> list =  m_mongo->search(txt);
+    QList<Item> listItem = convertMaptoItem(list);
+    m_listmodel->refreshFromMongo(QList<Item>());
+    m_listmodel->refreshFromMongo(listItem);
+}
+
+void InterfaceUI::clearSearch()
+{
+   QList<QVariantMap> list= m_mongo->data();
+    QList<Item> Items =convertMaptoItem(list);
+   m_listmodel->refreshFromMongo(Items);
+}
+
+void InterfaceUI::update( int id,const QString &fname, const QString &lname, const QString &phone, const QString &email)
+{
+    m_mongo->updateItem(id,fname,lname,phone,email);
+    clearSearch();
+}
+
+void InterfaceUI::remove(int id)
+{
+    m_mongo->deleteItem(id);
+    clearSearch();
+}
+
+void InterfaceUI::removeAll()
+{
+    m_mongo->deleteall();
+    clearSearch();
+}
+
+QList<Item> InterfaceUI::convertMaptoItem(QList<QVariantMap> &list)
+{
+
+    QList<Item> listItem;
+
+    foreach (QVariantMap var, list) {
+        Item item;
+        item.FirstName = var["FirstName"].toString();
+        item.LastName = var["LastName"].toString();
+        item.email = var["email"].toString();
+        item.PhoneNumber = var["PhoneNumber"].toString();
+        item.id = var["id"].toInt();
+        listItem.append(item);
+    }
+    return listItem;
 }
--- Cpp/interfaceui.h
+++ Cpp/interfaceui.h
@@ -15,12 +15,23 @@ Q_INVOKABLE void get_data(const QString &fname,const QString &lname,
                             const QString &phone,const QString &email
                               );
 
+    Q_INVOKABLE void search(const QString &txt);
+    Q_INVOKABLE void clearSearch();
+Q_INVOKABLE void update(int id,const QString &fname,
+                        const QString &lname,
+                        const QString &phone,
+                        const QString &email);
+    Q_INVOKABLE void remove(int id);
+    Q_INVOKABLE void removeAll();
+
 signals:
 
 private:
     MongoManager* m_mongo;
     ListModel * m_listmodel;
     int m_id {0};
+
+    QList <Item> convertMaptoItem(QList<QVariantMap> &list);
 };
 
 #endif // INTERFACEUI_H
